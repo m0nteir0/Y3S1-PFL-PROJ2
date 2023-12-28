4ec8756015e34f73152bb4934dbd781044df5d35
gmonteiro <gbmonteiro24@gmail.com>
2023-12-28 16:18:11 +0000

    parser BoolExp almost done. Bug on Not and right associative

--- src/Parser_2.hs
+++ src/Parser_2.hs
@@ -43,7 +43,7 @@ data Bexp --boolean expressions
     -- | Or Bexp Bexp -- or node
     | NOT Bexp -- not node
     | EQa Aexp Aexp -- equal node --
-    | EQb Bexp Bexp -- equal node --
+    | EQb Bexp Bexp -- equal node 
     | LE Aexp Aexp -- less than or equal node
     deriving Show
 
@@ -95,22 +95,22 @@ parseSumOrProdOrInt tokens
 
 -- ========================
 -- parethesised expressions
-parseIntOrParenExpr :: [Token] -> Maybe (Aexp, [Token])
-parseIntOrParenExpr (IntTok n : restTokens)
+parseIntOrPar :: [Token] -> Maybe (Aexp, [Token])
+parseIntOrPar (IntTok n : restTokens)
   = Just (NUM (fromIntegral n), restTokens)
-parseIntOrParenExpr (OpenTok : restTokens1)
+parseIntOrPar (OpenTok : restTokens1)
   = case parseSumOrProdOrIntOrPar restTokens1 of
     Just (expr, CloseTok : restTokens2) ->
       Just (expr, restTokens2)
     Just _ -> Nothing -- no closing paren
     Nothing -> Nothing
-parseIntOrParenExpr tokens = Nothing
+parseIntOrPar tokens = Nothing
 
 
 -- Parsing products or parenthesised expressions
 parseProdOrIntOrPar :: [Token] -> Maybe (Aexp, [Token])
 parseProdOrIntOrPar tokens
-  = case parseIntOrParenExpr tokens of
+  = case parseIntOrPar tokens of
     Just (expr1, MultTok : restTokens1) ->
       case parseProdOrIntOrPar restTokens1 of
         Just (expr2, restTokens2) ->
@@ -142,44 +142,120 @@ parseSumOrProdOrIntOrPar tokens
 --   case parseSumOrProdOrIntOrPar tokens of
 --     Just (expr, []) -> expr
 --     _ -> error "Parse error"
-parseAexp :: [Token] -> Maybe Aexp
+parseAexp :: [Token] -> Maybe (Aexp, [Token])
 parseAexp tokens =
   case parseSumOrProdOrIntOrPar tokens of
-    Just (expr, []) -> Just expr
-    _ -> Nothing
+    Just (expr, restTokens) -> Just (expr, restTokens)
+    _ -> Nothing  -- !WARNING: may need a result -> result here
 
 
 
 -- ========================
-parseBoolorParent :: [Token] -> Maybe (Bexp, [Token])
-parseBoolorParent (TruTok : restTokens) = Just (BOOL True, restTokens)
-parseBoolorParent (FalsTok : restTokens) = Just (BOOL False, restTokens)
-parseBoolorParent (OpenTok : restTokens1) =
-  case parseOrAndNotOrRelOrBool restTokens1 of -- !TODO: change function name
+parseBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseBoolOrPar (TruTok : restTokens) = Just (BOOL True, restTokens)
+parseBoolOrPar (FalsTok : restTokens) = Just (BOOL False, restTokens)
+parseBoolOrPar (OpenTok : restTokens1) =
+  case parseAndOrEqBOrNotOrEqAOrLTOrBoolOrPar restTokens1 of -- !TODO: change function name
     Just (expr, CloseTok : restTokens2) ->
       Just (expr, restTokens2)
     Just _ -> Nothing -- no closing paren
     Nothing -> Nothing
 
 
--- !TODO: figure out what functions are needed
+parseEqAOrLTOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseEqAOrLTOrBoolOrPar tokens =
+  case parseAexp tokens of --see if there is an integer expression
+    Just (expr1, LeTok : restTokens1) -> --if there is, check if there is a less than or equal operator next
+      case parseAexp restTokens1 of -- if there is, check if there is another integer expression
+        Just (expr2, restTokens2) -> Just (LE expr1 expr2, restTokens2) -- if there is, return the expression
+        Nothing -> Nothing
+    -- Just result -> Just result 
+    Just (expr1, EqITok : restTokens1) -> --if there is, check if there is a less than or equal operator next
+      case parseAexp restTokens1 of -- if there is, check if there is another integer expression
+        Just (expr2, restTokens2) -> Just (EQa expr1 expr2, restTokens2) -- if there is, return the expression
+        Nothing -> Nothing
+    Nothing -> parseBoolOrPar tokens -- if there is no integer expression, check if there is a boolean expression
+  -- case parseBoolOrPar tokens -- será que é necessario?
+
+-- parseIEqORIneqOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+-- -- parseIEqORIneqOrBoolOrPar tokens
+
+parseNotOrEqAOrLTOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseNotOrEqAOrLTOrBoolOrPar tokens =
+  case parseEqAOrLTOrBoolOrPar tokens of
+    Just (expr1, NotTok : restTokens1) -> Just (NOT expr1, restTokens1)      
+    result -> result
 
 
-parseBexp :: [Token] -> Maybe Bexp
+parseEqBOrNotOrEqAOrLTOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseEqBOrNotOrEqAOrLTOrBoolOrPar tokens =
+  case parseNotOrEqAOrLTOrBoolOrPar tokens of
+    Just (expr1, EqBTok : restTokens1) ->
+      case parseEqBOrNotOrEqAOrLTOrBoolOrPar restTokens1 of
+        Just (expr2, restTokens2) -> Just (EQb expr1 expr2, restTokens2)
+        Nothing -> Nothing
+    result -> result
+
+
+    
+
+parseAndOrEqBOrNotOrEqAOrLTOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseAndOrEqBOrNotOrEqAOrLTOrBoolOrPar tokens =
+  case parseEqBOrNotOrEqAOrLTOrBoolOrPar tokens of
+    Just (expr1, AndTok : restTokens1) ->
+      case parseAndOrEqBOrNotOrEqAOrLTOrBoolOrPar restTokens1 of
+        Just (expr2, restTokens2) -> Just (AND expr1 expr2, restTokens2)
+        Nothing -> Nothing
+    Just result -> Just result
+    Nothing -> Nothing
+
+parseBexp :: [Token] -> Maybe (Bexp, [Token])
 parseBexp tokens =
-  case parseOrAndNotOrRelOrBool tokens of -- !TODO: change function name  
-    Just (expr, []) -> Just expr
+  case parseAndOrEqBOrNotOrEqAOrLTOrBoolOrPar tokens of -- !TODO: change function name  
+    Just (expr, restTokens) -> Just (expr, restTokens)
     _ -> Nothing
 
 
-parseStm :: [Token] -> Stm
-parseStm tokens =
+-- parseStm :: [Token] -> Stm
+-- parseStm tokens =
 
 
 
-buildData:: [Token] -> [Stm]
-buildData tokens = [AExp (parseAexp tokens)] -- !NOTE: just tot test.
--- needs to read to EoTTok and call parseStm
+-- buildData:: [Token] -> [Stm]
+-- buildData tokens = 
+--   case parseBexp tokens of
+--     Just (expr, restTokens) -> expr : buildData restTokens
+--     _ -> []
 
-parse :: String -> [Stm]
-parse = buildData . lexer
+-- -- needs to read to EoTTok and call parseStm
+
+-- parse :: String -> [Stm]
+-- parse = buildData . lexer
+
+
+
+-- ========================
+  -- examples of use AEXP and BEXP in expressions
+-- 3+2 <= 5
+-- 3+2 <= 5 and 3+2 <= 5
+-- 3+2==10   -- False
+-- False and True
+-- False and True = False
+-- False and True = True
+-- 3+2==10 = False   (boolean equality uses =, while integer equality uses ==)
+
+
+-- !TODO: figure out what functions are needed
+--  In boolean expressions, two instances of the same operator are also computed
+-- from left to right. The order of precedence for the operations is (with the first
+-- one being executed first): integer inequality (≤), integer equality (==), logical
+-- negation (not), boolean equality (=), logical conjunction (and). For instance,
+-- not True and 2 ≤ 5 = 3 == 4 is equivalent to (not True) and ((2 ≤ 5) = (3
+-- == 4))
+-- ORDER OF PRECEDENCE:
+-- 0. Boolean Value & Arithmetic expressions
+-- 1. integer inequality (≤)      -> arithmetic expressions
+-- 2. integer equality (==)     -> arithmetic expressions
+-- 3. logical negation (not)
+-- 4. boolean equality (=)
+-- 5. logical conjunction (and)   -> feito primeiro
